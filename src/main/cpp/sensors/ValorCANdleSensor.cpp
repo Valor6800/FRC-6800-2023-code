fa1cf01062faa9d86837fbc4404d031c0efbfe10
Michael Ray <mray190@gmail.com>
2023-01-27 17:06:24 -0600

    fix: Update CANdle to working code

--- src/main/cpp/sensors/ValorCANdleSensor.cpp
+++ src/main/cpp/sensors/ValorCANdleSensor.cpp
@@ -17,14 +17,15 @@ ValorCANdleSensor::ValorCANdleSensor(frc::TimedRobot *_robot, int _ledCount, int
     candle(_canID, _canbus),
     ledCount(_ledCount),
     currentColor(toRGB(VALOR_GOLD)),
-    activeAnimation(NULL)
+    activeAnimation(NULL),
+    activeAnimationType(AnimationType::None)
 {
     reset();
 
     ctre::phoenix::led::CANdleConfiguration config;
     // Should match the type of LED strip connected to the CANdle
     config.stripType = ctre::phoenix::led::LEDStripType::GRB;
-    config.brightnessScalar = 0.5;
+    config.brightnessScalar = 0.75;
     config.statusLedOffWhenActive = true;
     config.disableWhenLOS = false;
     // If the 12V line should be on, off, or modulated (for single LED colors)
@@ -47,11 +48,16 @@ ValorCANdleSensor::~ValorCANdleSensor()
     clearAnimation();
 }
 
+void ValorCANdleSensor::setColor(int r, int g, int b)
+{
+    candle.SetLEDs(r,g,b);
+}
+
 void ValorCANdleSensor::setColor(int color)
 {
     clearAnimation();
     currentColor = toRGB(color);
-    candle.SetLEDs(currentColor.red, currentColor.green, currentColor.blue);
+    candle.SetLEDs(currentColor.red, currentColor.green, currentColor.blue,0,0,ledCount);
 }
 
 void ValorCANdleSensor::setAnimation(ValorCANdleSensor::AnimationType animation)
@@ -60,27 +66,29 @@ void ValorCANdleSensor::setAnimation(ValorCANdleSensor::AnimationType animation)
     int speed = 1;
     int brightness = 1;
 
-    clearAnimation();
-    if (animation == AnimationType::ColorFlow)
-        activeAnimation = new ctre::phoenix::led::ColorFlowAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::ColorFlowAnimation::Forward, startLED);
-    else if (animation == AnimationType::Fire)
-        activeAnimation = new ctre::phoenix::led::FireAnimation(brightness,speed,ledCount,1,1,false,startLED);
-    else if (animation == AnimationType::Larson)
-        activeAnimation = new ctre::phoenix::led::LarsonAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::LarsonAnimation::Front,5,startLED);
-    else if (animation == AnimationType::Rainbow)
-        activeAnimation = new ctre::phoenix::led::RainbowAnimation(brightness,speed,ledCount,false,startLED);
-    else if (animation == AnimationType::RgbFade)
-        activeAnimation = new ctre::phoenix::led::RgbFadeAnimation(brightness,speed,ledCount,startLED);
-    else if (animation == AnimationType::SingleFade)
-        activeAnimation = new ctre::phoenix::led::SingleFadeAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,startLED);
-    else if (animation == AnimationType::Strobe)
-        activeAnimation = new ctre::phoenix::led::StrobeAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,startLED);
-    else if (animation == AnimationType::Twinkle)
-        activeAnimation = new ctre::phoenix::led::TwinkleAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::TwinkleAnimation::TwinklePercent::Percent100,startLED);
-    else if (animation == AnimationType::TwinkleOff)
-        activeAnimation = new ctre::phoenix::led::TwinkleOffAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::TwinkleOffAnimation::TwinkleOffPercent::Percent100,startLED);
-    else
-        setColor(VALOR_GOLD);
+    if (animation != activeAnimationType) {
+        clearAnimation();
+        activeAnimationType = animation;
+
+        if (animation == AnimationType::ColorFlow)
+            activeAnimation = new ctre::phoenix::led::ColorFlowAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::ColorFlowAnimation::Forward, startLED);
+        else if (animation == AnimationType::Fire)
+            activeAnimation = new ctre::phoenix::led::FireAnimation(brightness,speed,ledCount,1,1,false,startLED);
+        else if (animation == AnimationType::Larson)
+            activeAnimation = new ctre::phoenix::led::LarsonAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::LarsonAnimation::Front,5,startLED);
+        else if (animation == AnimationType::Rainbow)
+            activeAnimation = new ctre::phoenix::led::RainbowAnimation(brightness,speed,ledCount,false,startLED);
+        else if (animation == AnimationType::RgbFade)
+            activeAnimation = new ctre::phoenix::led::RgbFadeAnimation(brightness,speed,ledCount,startLED);
+        else if (animation == AnimationType::SingleFade)
+            activeAnimation = new ctre::phoenix::led::SingleFadeAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,startLED);
+        else if (animation == AnimationType::Strobe)
+            activeAnimation = new ctre::phoenix::led::StrobeAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,startLED);
+        else if (animation == AnimationType::Twinkle)
+            activeAnimation = new ctre::phoenix::led::TwinkleAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::TwinkleAnimation::TwinklePercent::Percent100,startLED);
+        else if (animation == AnimationType::TwinkleOff)
+            activeAnimation = new ctre::phoenix::led::TwinkleOffAnimation(currentColor.red,currentColor.green,currentColor.blue,0,speed,ledCount,ctre::phoenix::led::TwinkleOffAnimation::TwinkleOffPercent::Percent100,startLED);
+    }
 
     if (activeAnimation)
         candle.Animate(*activeAnimation);
@@ -88,6 +96,7 @@ void ValorCANdleSensor::setAnimation(ValorCANdleSensor::AnimationType animation)
 
 void ValorCANdleSensor::clearAnimation()
 {
+    activeAnimationType = AnimationType::None;
     if (activeAnimation != NULL) {
         candle.ClearAnimation(0);
         delete activeAnimation;
