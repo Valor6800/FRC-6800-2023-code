509cfddfac336d1418a862489e1a692f132c07c7
Amaan Syed <amaanpsyed@gmail.com>
2023-02-03 14:32:10 -0600

    Intake current sensor added, second motor added, full AI AO AD completed

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -24,16 +24,20 @@ void Intake::resetState()
 
 void Intake::init()
 {
+    intakeMotor.setupFollower(11,true);
     //table->PutNumber("Motor 1 Max Speed", motor1MaxSpeed);
     // table->PutNumber("Motor 2 Max Speed", motor2MaxSpeed);
     // table->PutNumber("Motor 3 Max Speed", 0.0); 
-
+    currySensor.setSpikeSetpoint(20);
+    currySensor.setGetter([this]() { return intakeMotor.getCurrent(); });
+    currySensor.setSpikeCallback([this]() { state.intakeState = SPIKED;});
     resetState();
 }
 
 void Intake::assessInputs()
 {
     if (driverGamepad->GetAButtonPressed()){
+            // works with elevarm locations in order to outtake at speed for cone/cube
             if (ELEVARM_CONE){
                 state.intakeState = OUTTAKE_CONE;
             } else if (ELEVARM_CUBE){
@@ -41,7 +45,7 @@ void Intake::assessInputs()
             }
     } else if (operatorGamepad->rightTriggerActive()){
         state.intakeState = OUTTAKE;
-    }else if (true){
+    }else if (state.intakeState != SPIKED){
         //need to add current sensing abilities here?
         if (driverGamepad->GetLeftBumperPressed() || driverGamepad->GetRightBumperPressed() || 
         operatorGamepad->leftTriggerActive()){
@@ -49,27 +53,26 @@ void Intake::assessInputs()
         state.intakeState = INTAKE;
         }
         
-    } else {
+    } else if(state.intakeState == SPIKED){
+
+        if (!driverGamepad->GetLeftBumperPressed() && !driverGamepad->GetRightBumperPressed() && 
+        !operatorGamepad->leftTriggerActive()){
+        //move elevarm to front position
+        state.intakeState = DISABLED;
+        } 
+    }
+     else {
 
         state.intakeState = DISABLED;
     }
+    //need to check when button is released in order to reset Spiked
+    //might have to add a 
     // state.leftStickSpeed = driverGamepad->leftStickY(2);
     // state.rightStickSpeed = driverGamepad->rightStickY(2);
 }
 void Intake::analyzeDashboard()
 {
-
-    if (state.intakeState == DISABLED){
-        intakeMotor.setSpeed(0);
-    } else if (state.intakeState == OUTTAKE_CONE){
-        intakeMotor.setSpeed(-0.5);
-    } else if (state.intakeState == OUTTAKE_CUBE){
-        intakeMotor.setSpeed(-0.25);
-    } else if (state.intakeState == INTAKE){
-        intakeMotor.setSpeed(-0.5);
-    } else {
-        intakeMotor.setSpeed(0);
-    }
+    table->PutNumber("Intake Current", currySensor.getSensor());
     // motor1MaxSpeed = table->GetNumber("Motor 1 Max Speed", 0);
     // motor2MaxSpeed = table->GetNumber("Motor 2 Max Speed", 0);
     // table->GetNumber("Motor 3 Max Speed", motor3MaxSpeed);
@@ -78,14 +81,14 @@ void Intake::analyzeDashboard()
 
 void Intake::assignOutputs()
 { 
-    if (state.intakeState == DISABLED){
+    if (state.intakeState == DISABLED || state.intakeState == SPIKED){
         intakeMotor.setSpeed(0);
     } else if (state.intakeState == OUTTAKE_CONE){
         intakeMotor.setSpeed(-0.5);
     } else if (state.intakeState == OUTTAKE_CUBE){
         intakeMotor.setSpeed(-0.25);
     } else if (state.intakeState == INTAKE){
-        intakeMotor.setSpeed(-0.5);
+        intakeMotor.setSpeed(0.5);
     } else {
         intakeMotor.setSpeed(0);
     }
