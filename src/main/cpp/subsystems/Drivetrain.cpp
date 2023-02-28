6fdda3a79ae332e921a9a43adabe9a296d2b122a
Hillo111 <stas.gannutin@gmail.com>
2023-02-27 19:33:24 -0600

    change end x vel 0->0.1

--- src/main/cpp/subsystems/Drivetrain.cpp
+++ src/main/cpp/subsystems/Drivetrain.cpp
@@ -435,7 +435,7 @@ frc2::FunctionalCommand* Drivetrain::getAutoLevel(){
             } else if (state.abovePitchThreshold) {
                 if (pigeon.GetPitch() < 10.7 ){
                     state.isLeveled = true;
-                    state.xSpeed = 0.0;
+                    state.xSpeed = 0.1;
                 }else if(pigeon.GetPitch() < 16){
                     state.xSpeed = -0.15;
                 }else{
