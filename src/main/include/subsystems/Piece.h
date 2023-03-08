4305de7ac446af81164e0a21bbaa1328d629fa72
Amaan Syed <amaanpsyed@gmail.com>
2023-03-08 16:23:01 -0600

    made piece and direction.h a single enum

--- src/main/include/subsystems/Piece.h
+++ src/main/include/subsystems/Piece.h
@@ -12,48 +12,10 @@
 #include <frc2/command/FunctionalCommand.h>
 class Piece : public ValorSubsystem
 {
-public:
-     /**
-      * @brief Construct a new Intake object
-      * 
-      * @param robot Top level robot object to parse out smart dashboard and table information
-      */
-     Piece(frc::TimedRobot *robot);
-
-     /**
-      * @brief Destroy the Intake object
-      * 
-      * Intake objects have member objects on the heap - need a destructor to take care of memory on destruction
-      */
-     ~Piece();
-
-     /**
-      * @brief Initialize the Intake
-      * 
-      */
-     void init();
-
-     void assessInputs();
-     void analyzeDashboard();
-     void assignOutputs();
-
-     void resetState();
-
-     void InitSendable(wpi::SendableBuilder& builder);
-
-     
-
-
-     //this state will come from elevarm, elevarm not currently connected to dev
-
+public:  
      enum PieceState{
         CONE,
         CUBE
      };
 
-     struct x
-     {
-         PieceState pieceState;
-
-     }state, prevState;
 };
