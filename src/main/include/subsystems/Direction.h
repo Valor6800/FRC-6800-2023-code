da08e942f9ae5201e4854aec16f13b05335c6961
Amaan Syed <amaanpsyed@gmail.com>
2023-03-08 16:23:01 -0600

    made piece and direction.h a single enum

--- src/main/include/subsystems/Direction.h
+++ src/main/include/subsystems/Direction.h
@@ -13,47 +13,8 @@
 class Direction : public ValorSubsystem
 {
 public:
-     /**
-      * @brief Construct a new Intake object
-      * 
-      * @param robot Top level robot object to parse out smart dashboard and table information
-      */
-     Direction(frc::TimedRobot *robot);
-
-     /**
-      * @brief Destroy the Intake object
-      * 
-      * Intake objects have member objects on the heap - need a destructor to take care of memory on destruction
-      */
-     ~Direction();
-
-     /**
-      * @brief Initialize the Intake
-      * 
-      */
-     void init();
-
-     void assessInputs();
-     void analyzeDashboard();
-     void assignOutputs();
-
-     void resetState();
-
-     void InitSendable(wpi::SendableBuilder& builder);
-
-     
-
-
-     //this state will come from elevarm, elevarm not currently connected to dev
-
      enum DirectionState{
         FRONT,
         BACK
      };
-
-     struct x
-     {
-         DirectionState directionState;
-
-     }state, prevState;
 };
